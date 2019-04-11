36e1e81cc63953801965e1af87d88aba4b0a09b7
lyh <liuyonghua1996@outlook.com>
2019-04-11 12:00:00 +0800

    更换图片

--- src/components/SunQuestion.vue
+++ src/components/SunQuestion.vue
@@ -5,14 +5,20 @@
         </div>
         <transition name="fade">
             <div class="img" v-if="showUnKnow">
-                <img src="../assets/sun-rise.jpg" alt="" :class="{active:active ==='a'}">
-                <img src="../assets/sun-down.jpeg" alt="" :class="{active:active ==='d'}">
+                <img src="../assets/sun-rise.png" alt="" :class="{active:active ==='a'}">
+                <img src="../assets/sun-down.png" alt="" :class="{active:active ==='d'}">
             </div>
         </transition>
         <transition name="fade">
             <div class="choose" v-if="showUnKnow">
-                <div class="choose-left" @click="chooseAnswer('a')">日出</div>
-                <div class="choose-right" @click="chooseAnswer('d')">日落</div>
+                <div class="choose-left" @click="chooseAnswer('a')">
+                    <img src="../assets/unselected.png" alt="">
+                    日出
+                </div>
+                <div class="choose-right" @click="chooseAnswer('d')">
+                    <img src="../assets/unselected.png" alt="">
+                    日落
+                </div>
             </div>
         </transition>
 
@@ -41,7 +47,7 @@
                     clearInterval(timer);
                     that.showUnKnow = true
                 }
-            }, 500);
+            }, 200);
         },
         methods: {
             chooseAnswer: function (a) {
@@ -55,30 +61,32 @@
     #sunQuestion {
         width: 100%;
         height: 100%;
-        background: url("../assets/1.png");
+        background: url("../assets/bg-1.png");
         background-size: 100% 100%;
     }
 
     .title {
         width: 100%;
-        height: 1rem;
-        padding: 2rem 0 0.2rem 0.6rem;
+        height: 1.5rem;
+        padding: 2.08rem 0 0.2rem 0.56rem;
         font-size: 0.34rem;
         color: #fff;
-        letter-spacing: 0.02rem;
+        letter-spacing: 0.03rem;
         font-weight: 300;
     }
 
     .img {
-        width: 100%;
-        height: 3rem;
+        width: 88%;
+        height: 3.42rem;
+        margin: 0 auto;
+        padding: 0.15rem 0;
         display: flex;
         justify-content: space-around;
     }
 
     .img img {
-        width: 2rem;
-        opacity: 0.8;
+        width: 2.58rem;
+        opacity: 0.6;
         transition: opacity .5s;
         box-shadow: -1px 1px 3px 5px rgba(0, 0, 0, 0.8);
     }
@@ -91,7 +99,8 @@
         width: 100%;
         height: 1.5rem;
         line-height: 1.5rem;
-        font-size: 0.26rem;
+        font-size: 0.36rem;
+        font-weight: 300;
         color: #fff;
         display: flex;
         justify-content: space-around;
@@ -105,6 +114,14 @@
     .choose .choose-right {
         width: 2rem;
         text-align: center;
+    }
+    .choose-left img{
+         width: 0.26rem;
+         height: 0.28rem;
+     }
+    .choose-right img{
+        width: 0.26rem;
+        height: 0.28rem;
     }
 
     .fade-enter-active, .fade-leave-active {
